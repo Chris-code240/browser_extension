7983783e68da20a1c44c62e5db872672e1723036
Chris-code240 <duah.marfochristian@gmail.com>
2023-07-25 21:55:31 +0100

    Image content request configured

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
   let videos = document.querySelectorAll('video')
+  let images = document.querySelectorAll('img')
+  let contents = document.querySelectorAll('img, video')
+  let counter = 0
   videos.forEach((video, index)=>{
     fetch('http://localhost:5000/analyze',{
       body:JSON.stringify({"path":video.src}),
@@ -8,7 +11,6 @@
     }).then((res)=>res.json()).then((jres)=>{
       if(jres.success && jres.trigger){
         video.parentElement.classList.add('hidden')
-
       }
     }).catch((e)=>console.log(e))
 
@@ -18,6 +20,43 @@
         videos.forEach(video=>{
           if(video.parentElement.classList.contains('hidden')){
             video.parentElement.classList.remove('hidden')
+          }
+        })
+      }
+    }
+  })
+
+  contents.forEach((ele, index)=>{
+    if(ele.tagName == "img"){
+      fetch('http://localhost:5000/analyze-image',{
+        body:JSON.stringify({"path":ele.src}),
+        headers: {"Content-type":"application/json"},
+        method: "POST"
+      }).then((res)=>res.json()).then((jres)=>{
+        if(jres.success && jres.trigger){
+          ele.parentElement.classList.add('hidden')
+          counter += 1
+        }
+      }).catch((e)=>console.log(e))
+    }else{
+      fetch('http://localhost:5000/analyze',{
+        body:JSON.stringify({"path":ele.src}),
+        headers: {"Content-type":"application/json"},
+        method: "POST"
+      }).then((res)=>res.json()).then((jres)=>{
+        if(jres.success && jres.trigger){
+          ele.parentElement.classList.add('hidden')
+          counter += 1
+        }
+      }).catch((e)=>console.log(e))      
+    }
+
+    if(index + 1 == videos.length && counter > 0){
+      let con = confirm("Some content are possible epileptic triggers. Do you want to see them anyway?")
+      if (con){
+        contents.forEach(ele=>{
+          if(ele.parentElement.classList.contains('hidden')){
+            ele.parentElement.classList.remove('hidden')
           }
         })
       }
